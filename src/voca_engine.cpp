ec38907818e12a3bab5aa5e9c04c8c0f0e3bf5a4
Kim-JeongHan <kimjh9813@naver.com>
2026-01-20 15:41:34 +0900

    Fix: Correct variable name in isCorrect function for clarity

--- src/voca_engine.cpp
+++ src/voca_engine.cpp
@@ -19,9 +19,9 @@ bool VocaTestEngine::isCorrect(const std::string& answer, const std::string& cor
             return true;
         }
 
-        for (const auto& and : answers) {
+        for (const auto& answer : answers) {
             for (const auto& resp : responses) {
-                if (and == resp) {
+                if (answer == resp) {
                     return true;
                 }
             }
